fcf6bb85690ffa1569a2dbd1e60ff811eab31c81
Cesar Duque Leal Silva <102749632+Cesar-Duque@users.noreply.github.com>
2022-06-05 00:37:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var dvTempo 
+var btnStart
+var btnPause
+var btnReset
+var tempo = 0
+var id
+
+init()
+
+function init() {
+  dvTempo = document.getElementById("tempo")
+  
+  btnStart = document.getElementById("start")
+  btnStart.onclick = start
+  
+  btnPause = document.getElementById("pause")
+  btnPause.disabled = true
+  btnPause.onclick = pause
+  
+  btnReset = document.getElementById("reset")
+  btnReset.disabled = true
+  btnReset.onclick = reset 
+}
+
+function update() {
+  tempo = tempo + 0.1
+  dvTempo.innerHTML = tempo.toFixed(1) + "s"
+}
+
+function start() {
+  console.log("Cronômetro Iniciado")
+  id = setInterval(update, 100)
+  btnStart.disabled = true
+  btnPause.disabled = false
+  btnReset.disabled = false
+}
+
+function pause() {
+  console.log("Cronômetro Pausado")
+  btnStart.disabled = false
+  btnPause.disabled = true
+  clearInterval(id)
+}
+
+function reset() {
+  console.log("Cronômetro Resetado")
+  btnStart.disabled = false
+  btnPause.disabled = true
+  btnReset.disabled = true
+  clearInterval(id)
+  tempo = 0
+  dvTempo.innerHTML ="0.0s"
+}
+
